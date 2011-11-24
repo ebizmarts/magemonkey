9fd6e913b1c650338c277ce26214ee65ba3a967e
di Pola <ignacio@ebizmarts.com>
2011-11-24 16:05:54 -0200

    Added account info to settings page

---
+++ app/code/community/Ebizmarts/MageMonkey/Model/System/Config/Source/Account.php
@@ -0,0 +1,25 @@
+<?php
+
+class Ebizmarts_MageMonkey_Model_System_Config_Source_Account {
+
+    protected $_account_details = FALSE;
+
+    public function __construct() {
+        if (!$this->_account_details) {
+            $this->_account_details = Mage::getSingleton('monkey/api')
+                    ->getaccountdetails();
+        }
+    }
+
+    public function toOptionArray() {
+        if($this->_account_details){
+            return array(
+                array('value' => 0, 'label' => 'Username: ' . $this->_account_details['username']),
+                array('value' => 1, 'label' => 'Plan type: ' . $this->_account_details['plan_type'])
+            );
+        }else{
+            return array(array('value' => '', 'label' => Mage::helper('monkey')->__('--- Enter your API KEY first) ---')));
+        }
+    }
+
+}
