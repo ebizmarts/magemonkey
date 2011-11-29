2d49945ce0ba1d0efe0d42cf6235b8d8675031a5
Pablo <pablos@ebizmarts.com>
2011-11-29 14:53:23 -0200

    list activity

---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Customer/Edit/Tab/Memberactivity.php
@@ -0,0 +1,73 @@
+<?php
+
+class Ebizmarts_MageMonkey_Block_Adminhtml_Customer_Edit_Tab_Memberactivity
+    extends Ebizmarts_MageMonkey_Block_Adminhtml_Memberactivity_Grid
+    implements Mage_Adminhtml_Block_Widget_Tab_Interface
+{
+
+    /**
+     * Columns, that should be removed from grid
+     *
+     * @var array
+     */
+    protected $_columnsToRemove = array('visitor_session_id', 'protocol', 'customer_id');
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setFilterVisibility(FALSE);
+        $this->setSaveParametersInSession(FALSE);
+    }
+
+    /**
+     * Defines after which tab, this tab should be rendered
+     *
+     * @return string
+     */
+    public function getAfter()
+    {
+        return 'orders';
+    }
+
+    /**
+     * Return Tab label
+     *
+     * @return string
+     */
+    public function getTabLabel()
+    {
+        return $this->__('MailChimp List Member Activity');
+    }
+
+    /**
+     * Return Tab title
+     *
+     * @return string
+     */
+    public function getTabTitle()
+    {
+        return $this->__('MailChimp List Member Activity');
+    }
+
+    /**
+     * Can show tab in tabs
+     *
+     * @return boolean
+     */
+    public function canShowTab()
+    {
+        $customer = Mage::registry('current_customer');
+        return (bool)$customer->getId();
+    }
+
+    /**
+     * Tab is hidden
+     *
+     * @return boolean
+     */
+    public function isHidden()
+    {
+    	//TODO: Hide if MageMonkey is disabled
+        return false;
+    }
+}
---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Memberactivity/Grid.php
@@ -0,0 +1,83 @@
+<?php
+
+class Ebizmarts_MageMonkey_Block_Adminhtml_Memberactivity_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('token_grid');
+        $this->setDefaultSort('id');
+        $this->setDefaultDir('DESC');
+    }
+
+    protected function _prepareCollection()
+    {
+    	$customer = Mage::registry('current_customer');
+    	$email    = $customer->getEmail();
+
+    	$api      = Mage::getSingleton('monkey/api', array('apikey' => Mage::helper('monkey')->getApiKey($customer->getStore())));
+    	$activity = array();
+    	$lists    = $api->listsForEmail($email);
+
+		if($lists){
+			foreach($lists as $list){
+				$activity []= $api->listMemberActivity($list, $email);
+			}
+
+			if(!empty($activity)){
+				$activityData = array();
+				foreach($activity as $act){
+
+					if(empty($act['data'][0])){
+						continue;
+					}
+
+					$activityData []= $act['data'];
+				}
+			}
+		}
+
+        $collection = Mage::getModel('monkey/custom_collection', array($activityData));
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+
+        $this->addColumn('action', array(
+            'header'=> Mage::helper('monkey')->__('Action'),
+            'index' => 'action',
+            'sortable' => false
+        ));
+        $this->addColumn('url', array(
+            'header'=> Mage::helper('monkey')->__('Url'),
+            'index' => 'url',
+            'sortable' => false
+        ));
+        $this->addColumn('bounce_type', array(
+            'header'=> Mage::helper('monkey')->__('Bounce Type'),
+            'index' => 'bounce_type',
+            'sortable' => false
+        ));
+        $this->addColumn('campaign_id', array(
+            'header'=> Mage::helper('monkey')->__('Campaign ID'),
+            'index' => 'campaign_id',
+            'sortable' => false
+        ));
+        $this->addColumn('timestamp', array(
+            'header'=> Mage::helper('monkey')->__('Timestamp'),
+            'index' => 'timestamp',
+            'sortable' => false
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    public function getRowUrl($row)
+    {
+        return false;
+    }
+
+}
