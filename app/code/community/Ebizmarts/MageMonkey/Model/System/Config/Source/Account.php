f5e7c346975c724a0d0041fe136cd921c6a4503e
di Pola <ignacio@ebizmarts.com>
2011-11-29 12:47:16 -0200

    Added more info to account settings details

--- app/code/community/Ebizmarts/MageMonkey/Model/System/Config/Source/Account.php
+++ app/code/community/Ebizmarts/MageMonkey/Model/System/Config/Source/Account.php
@@ -15,8 +15,9 @@ class Ebizmarts_MageMonkey_Model_System_Config_Source_Account
     public function toOptionArray() {
         if($this->_account_details){
             return array(
-                array('value' => 0, 'label' => 'Username: ' . $this->_account_details['username']),
-                array('value' => 1, 'label' => 'Plan type: ' . $this->_account_details['plan_type'])
+                array('value' => 0, 'label' => Mage::helper('monkey')->__('Username:') . ' ' . $this->_account_details['username']),
+                array('value' => 1, 'label' => Mage::helper('monkey')->__('Plan type:') . ' ' . $this->_account_details['plan_type']),
+                array('value' => 2, 'label' => Mage::helper('monkey')->__('Is in trial mode?:') . ' ' . ($this->_account_details['is_trial'] ? Mage::helper('monkey')->__('Yes') : Mage::helper('monkey')->__('No')))
             );
         }else{
             return array(array('value' => '', 'label' => Mage::helper('monkey')->__('--- Enter your API KEY first ---')));
