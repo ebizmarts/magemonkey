d1d8ab74f1410d530a6a5dc33637dcbc36ef3a9f
di Pola <ignacio@ebizmarts.com>
2011-12-01 11:48:00 -0200

    Added Checkout Subscribe model and translations

---
+++ app/code/community/Ebizmarts/MageMonkey/Model/System/Config/Source/Checkoutsubscribe.php
@@ -0,0 +1,19 @@
+<?php
+
+class Ebizmarts_MageMonkey_Model_System_Config_Source_Checkoutsubscribe
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => 1, 'label' => Mage::helper('monkey')->__('Enabled - Checked by default')),
+            array('value' => 2, 'label' => Mage::helper('monkey')->__('Enabled - Not Checked by default')),
+            array('value' => 3, 'label' => Mage::helper('monkey')->__('Enabled - Force subscription')),
+            array('value' => 0, 'label' => Mage::helper('monkey')->__('-- Disabled --'))
+        );
+    }
+}
