e87fc9c23d93dc99bd18e53f636f8b559b9d9e28
Pablo <pablos@ebizmarts.com>
2011-12-05 10:58:28 -0200

    adding order_id column on magemonkey_ecommerce360 table

--- app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-0.0.1-0.0.2.php
+++ app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-0.0.1-0.0.2.php
@@ -8,6 +8,7 @@ $installer->run("
 
 	CREATE TABLE IF NOT EXISTS `{$this->getTable('magemonkey_ecommerce360')}` (
 	  `id` int(10) unsigned NOT NULL auto_increment,
+      `order_id` int(10) unsigned NOT NULL,
 	  `order_increment_id` varchar(50) NOT NULL default '',
       `mc_campaign_id` varchar(255) NOT NULL default '',
       `mc_email_id` varchar(255) NOT NULL default '',
