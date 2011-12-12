48ff918f614f4117dc2410264c910aebb6e64be7
Pablo <pablos@ebizmarts.com>
2011-12-12 17:35:36 -0200

    batch Export

--- app/code/community/Ebizmarts/MageMonkey/Model/Cron.php
+++ app/code/community/Ebizmarts/MageMonkey/Model/Cron.php
@@ -30,31 +30,86 @@ class Ebizmarts_MageMonkey_Model_Cron
 		$job = $job->getFirstItem();
 
 		$lists = unserialize($job->getLists());
-		var_dump($lists, $job->getDataSourceEntity());
 
 		$collection = $this->_getEntityModel($job->getDataSourceEntity())
 						->setPageSize($this->_limit);
 
+		//Condition for chunk batch
+		if($job->getLastProcessedId()){
+			$collection->addFieldToFilter($this->_getId($job->getDataSourceEntity()), array('gt' => (int)$job->getLastProcessedId()));
+		}
+
 		$collection->load();
 
-		var_dump((string)$collection->getSelect());
+    	//var_dump((string)$collection->getSelect());
 
 		$batch = array();
 
-		foreach($lists as $list){
-			$store = $this->_helper()->getStoreByList($list);
+		foreach($lists as $listId){
+			$store = $this->_helper()->getStoreByList($listId);
 
-			if($store){
+			//if($store){
 
+				$api = Mage::getSingleton('monkey/api', array('store' => $store));
+
+				$processedCount = 0;
 				foreach($collection as $item){
+					$processedCount += 1;
 					$batch []= $this->_helper()->getMergeVars($item, TRUE);
 				}
 
-			}
-		}
+				//var_dump($batch);
 
-		if(count($batch) > 0){
-			var_dump($batch);die;
+				if(count($batch) > 0){
+
+					$job->setStatus('chunk_running')
+						->setUpdatedAt($this->_dbDate())
+						->save();
+
+					$vals = $api->listBatchSubscribe($listId, $batch, FALSE, TRUE, FALSE);
+
+					if ( is_null($api->errorCode) ){
+
+						$lastId = $collection->getLastItem()->getId();
+						$job->setLastProcessedId($lastId);
+						$job->setProcessedCount( ( $processedCount+$job->getProcessedCount() ));
+
+						/*if( $processedCount < $this->_limit ){
+							$job->setStatus('finished');
+						}*/
+
+						$job
+						->setUpdatedAt($this->_dbDate())
+						->save();
+
+					} else {
+
+						//TODO: Do something to handle errors
+
+					    /*echo "Batch Subscribe failed!\n";
+						echo "code:".$api->errorCode."\n";
+						echo "msg :".$api->errorMessage."\n";
+						die;*/
+						/*echo "added:   ".$vals['add_count']."\n";
+						echo "updated: ".$vals['update_count']."\n";
+						echo "errors:  ".$vals['error_count']."\n";
+						foreach($vals['errors'] as $val){
+							echo $val['email_address']. " failed\n";
+							echo "code:".$val['code']."\n";
+							echo "msg :".$val['message']."\n";
+						}
+						die;*/
+
+					}
+
+				}else{
+					$job
+					->setStatus('finished')
+					->setUpdatedAt($this->_dbDate())
+					->save();
+				}
+
+			//}
 		}
 
 		return $this;
@@ -67,7 +122,7 @@ class Ebizmarts_MageMonkey_Model_Cron
 		switch ($type) {
 			case 'newsletter_subscriber':
 				$model = Mage::getResourceSingleton('newsletter/subscriber_collection')
-							->showCustomerInfo(true)
+							//->showCustomerInfo(true)
             				->addSubscriberTypeField()
             				->showStoreInfo();
 				break;
@@ -85,9 +140,28 @@ class Ebizmarts_MageMonkey_Model_Cron
 		return $model;
 	}
 
+	protected function _getId($type)
+	{
+		$idFieldName = null;
+
+		switch ($type) {
+			case 'newsletter_subscriber':
+				$idFieldName = 'subscriber_id';
+				break;
+			default:
+				$idFieldName = 'id';
+		}
+
+		return $idFieldName;
+	}
+
 	protected function _helper()
 	{
 		return Mage::helper('monkey');
 	}
 
+	protected function _dbDate()
+	{
+		return Mage::getModel('core/date')->gmtDate();
+	}
 }
