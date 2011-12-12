3df48fa7b3a38d081edeee2ed8e5fe3707aabc9e
Pablo <pablos@ebizmarts.com>
2011-12-12 18:47:15 -0200

    adding new fields and marking fields as required on IMPORT form for bulksync

--- app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Bulksync/Import/Form.php
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Bulksync/Import/Form.php
@@ -26,6 +26,8 @@ class Ebizmarts_MageMonkey_Block_Adminhtml_Bulksync_Import_Form extends Mage_Adm
             'title'     => Mage::helper('monkey')->__('Choose Lists'),
             'name'      => 'list',
             'values'   => $lists,
+            'class' => 'required-entry',
+            'required' => true,
         ));
 
         $fieldset->addField('create_customers', 'checkbox', array(
@@ -33,6 +35,14 @@ class Ebizmarts_MageMonkey_Block_Adminhtml_Bulksync_Import_Form extends Mage_Adm
             'title'     => Mage::helper('monkey')->__('Create customer accounts'),
             'name'      => 'create_customers',
             'value'     => '1'
+        ));
+
+        $fieldset->addField('since', 'date', array(
+            'label'     => Mage::helper('monkey')->__('Retrieve data since'),
+            'title'     => Mage::helper('monkey')->__('Retrieve data since'),
+            'name'      => 'since',
+            'image'     => $this->getSkinUrl('images/grid-cal.gif'),
+            'format'    => Varien_Date::DATE_INTERNAL_FORMAT
         ));
 
         $fieldset->addField('direction', 'hidden', array(
