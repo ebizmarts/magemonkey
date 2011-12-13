a0dfa83135f7ad720d45c9428af4e38003eb0791
Pablo <pablos@ebizmarts.com>
2011-12-13 13:17:22 -0200

    new actions new features. Import Export

--- app/code/community/Ebizmarts/MageMonkey/controllers/Adminhtml/BulksyncController.php
+++ app/code/community/Ebizmarts/MageMonkey/controllers/Adminhtml/BulksyncController.php
@@ -38,6 +38,29 @@ class Ebizmarts_MageMonkey_Adminhtml_BulksyncController extends Mage_Adminhtml_C
         $this->renderLayout();
 	}
 
+	public function queueAction()
+	{
+		$this->_initAction();
+		$this->_title($this->__('Job Queue'));
+        $this->renderLayout();
+	}
+
+	public function importgridAction()
+	{
+        $this->loadLayout();
+        $this->getResponse()->setBody(
+            $this->getLayout()->createBlock('monkey/adminhtml_bulksync_queueImport_grid')->toHtml()
+        );
+	}
+
+	public function exportgridAction()
+	{
+        $this->loadLayout();
+        $this->getResponse()->setBody(
+            $this->getLayout()->createBlock('monkey/adminhtml_bulksync_queueExport_grid')->toHtml()
+        );
+	}
+
 	public function saveAction()
 	{
 		$request = $this->getRequest();
@@ -47,14 +70,32 @@ class Ebizmarts_MageMonkey_Adminhtml_BulksyncController extends Mage_Adminhtml_C
 			return;
 		}
 
-		$job = Mage::getModel('monkey/bulksyncExport')
+		$job = new Varien_Object;
+		if($request->getPost('direction') == 'import'){
+
+			$job = Mage::getModel('monkey/bulksyncImport')
 					->setStatus('idle')
-					->setLists(serialize($request->getParam('list')))
-					->setDataSourceEntity($request->getParam('data_source_entity'))
+					->setLists(serialize($request->getPost('list')))
+					->setImportTypes(serialize($request->getPost('import_types')))
+					->setCreateCustomer((int)$request->getPost('create_customers'));
+
+			if($request->getPost('since')){
+				$job->setSince($request->getPost('since') . ' 00:00:00');
+			}
+
+			$job->save();
+
+		}elseif($request->getPost('direction') == 'export'){
+
+			$job = Mage::getModel('monkey/bulksyncExport')
+					->setStatus('idle')
+					->setLists(serialize($request->getPost('list')))
+					->setDataSourceEntity($request->getPost('data_source_entity'))
 					->save();
+		}
 
 		if( $job->getId() ){
-			$this->_getSession()->addSuccess($this->__('Export job #%s was sucessfully scheduled.', $job->getId()));
+			$this->_getSession()->addSuccess($this->__('Job #%s was sucessfully scheduled.', $job->getId()));
 		}else{
 			$this->_getSession()->addError($this->__('Could not schedule job.'));
 		}
