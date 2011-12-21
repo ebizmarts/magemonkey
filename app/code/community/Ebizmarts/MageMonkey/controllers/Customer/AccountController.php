509d9eee2ee9ac81cb5ef68a62b0d7b54f105bf2
Pablo <pablos@ebizmarts.com>
2011-12-21 12:37:13 -0200

    Customer Account controller

--- app/code/community/Ebizmarts/MageMonkey/controllers/Customer/AccountController.php
+++ app/code/community/Ebizmarts/MageMonkey/controllers/Customer/AccountController.php
@@ -6,15 +6,41 @@
 class Ebizmarts_MageMonkey_Customer_AccountController extends Mage_Core_Controller_Front_Action
 {
 
+    /**
+     * Action predispatch
+     *
+     * Check customer authentication for some actions
+     */
+    public function preDispatch()
+    {
+        parent::preDispatch();
+
+        if (!$this->getRequest()->isDispatched()) {
+            return;
+        }
+
+        if (!$this->_getCustomerSession()->authenticate($this)) {
+            $this->setFlag('', 'no-dispatch', true);
+        }
+    }
+
+    /**
+     * Retrieve customer session model object
+     *
+     * @return Mage_Customer_Model_Session
+     */
+    protected function _getCustomerSession()
+    {
+        return Mage::getSingleton('customer/session');
+    }
+
 	public function indexAction()
 	{
-		//TODO: Check that customer is logged in
+        $this->loadLayout();
+        $this->_initLayoutMessages('customer/session');
 
-		//Multiple lists, Interest groups
-
-		$this->getLayout();
-
-		$this->renderLayout();
+        $this->getLayout()->getBlock('head')->setTitle($this->__('MailChimp'));
+        $this->renderLayout();
 	}
 
 }
