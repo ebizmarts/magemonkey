c05b91a56ad635d6df6a5a040f44da7e905571c7
Pablo <pablos@ebizmarts.com>
2011-12-23 17:10:33 -0200

    adding renderers

---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Renderer/Importypes.php
@@ -0,0 +1,17 @@
+<?php
+
+class Ebizmarts_MageMonkey_Block_Adminhtml_Renderer_Importypes extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+    /**
+     * Renders grid column
+     *
+     * @param   Varien_Object $row
+     * @return  string
+     */
+    public function render(Varien_Object $row)
+    {
+    	$value = $this->_getValue($row);
+
+        return implode(', ', $row->statuses());
+    }
+}
---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Renderer/Lists.php
@@ -0,0 +1,24 @@
+<?php
+
+class Ebizmarts_MageMonkey_Block_Adminhtml_Renderer_Lists extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+    /**
+     * Renders grid column
+     *
+     * @param   Varien_Object $row
+     * @return  string
+     */
+    public function render(Varien_Object $row)
+    {
+    	$value = $this->_getValue($row);
+
+		$lists = Mage::getSingleton('monkey/api')
+							->lists(array('list_id' => implode(', ', $row->lists())));
+		$listsNames = array();
+		foreach($lists['data'] as $list){
+			$listsNames []= $list['name'];
+		}
+
+        return implode(', ', $listsNames);
+    }
+}
