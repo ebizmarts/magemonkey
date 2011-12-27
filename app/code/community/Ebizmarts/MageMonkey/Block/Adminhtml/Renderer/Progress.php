3f15af6a8ad7e08cb56284d5263c0ca1b2a73bc8
Pablo <pablos@ebizmarts.com>
2011-12-27 11:26:00 -0200

    adding new Renderer for progress bar on bulk sync jobs

---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Renderer/Progress.php
@@ -0,0 +1,14 @@
+<?php
+
+class Ebizmarts_MageMonkey_Block_Adminhtml_Renderer_Progress extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+    /**
+     * Renders grid column
+     * @param   Varien_Object $row
+     * @return  string
+     */
+    public function render(Varien_Object $row)
+    {
+        return $this->helper('monkey')->progressbar($row->getData('processed_count'), $row->getData('total_count'));
+    }
+}
