261a6ef29c28fda10f558be5dafaa5740752e6e8
Pablo Benitez <pablos@ebizmarts.com>
2012-01-17 14:29:42 -0200

    Mandrill API Wrapper

---
+++ app/code/community/Ebizmarts/MageMonkey/Model/TransactionalEmail/MANDRILL.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * MailChimp MANDRILL API wrapper
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_MageMonkey
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ */
+class Ebizmarts_MageMonkey_Model_TransactionalEmail_MANDRILL
+{
+
+}
