1dcd89a7b889c8b46983d670487f798dcb13eaf9
Pablo Benitez <pablos@ebizmarts.com>
2012-01-18 18:51:00 -0200

    adding files

---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Transactionalemail/Newemail.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Add valid email to Transactional email service
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_MageMonkey
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ */
+class Ebizmarts_MageMonkey_Block_Adminhtml_Transactionalemail_Newemail extends Mage_Adminhtml_Block_Widget_Form_Container
+{
+
+    public function __construct()
+    {
+        $this->_controller = 'adminhtml_transactionalemail';
+        $this->_blockGroup = 'monkey';
+        $this->_mode       = 'newemail';
+
+        parent::__construct();
+
+        $this->_updateButton('save', 'label', Mage::helper('monkey')->__('Add'));
+    }
+
+    public function getHeaderText()
+    {
+    	return Mage::helper('monkey')->__('Add valid email address');
+    }
+
+}
---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Transactionalemail/Newemail/Form.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Add new email to Transactional Email service FORM
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_MageMonkey
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ */
+class Ebizmarts_MageMonkey_Block_Adminhtml_Transactionalemail_Newemail_Form extends Mage_Adminhtml_Block_Widget_Form
+{
+
+    /**
+     * Prepare form before rendering HTML
+     *
+     * @return Mage_Adminhtml_Block_Widget_Form
+     */
+    protected function _prepareForm()
+    {
+        $form = new Varien_Data_Form(array(
+            'id'        => 'edit_form',
+            'action'    => $this->getUrl('*/*/validateEmail'),
+            'method'    => 'post'
+        ));
+
+        $fieldset   = $form->addFieldset('newemail_data', array(
+            'legend'    => Mage::helper('monkey')->__('New email')
+        ));
+
+        $fieldset->addField('email_address', 'text', array(
+            'label'     => Mage::helper('monkey')->__('Email address'),
+            'title'     => Mage::helper('monkey')->__('Email address'),
+            'name'      => 'email_address',
+            'class' => 'validate-email',
+            'required' => true,
+        ));
+
+        $fieldset->addField('service', 'hidden', array(
+            'name'     => 'service',
+            'value'    => $this->getRequest()->getParam('service'),
+        ));
+        $fieldset->addField('store', 'hidden', array(
+            'name'     => 'store',
+            'value'    => $this->getRequest()->getParam('store', 0),
+        ));
+
+        $form->setUseContainer(true);
+        $this->setForm($form);
+
+		return parent::_prepareForm();
+
+    }
+
+}
