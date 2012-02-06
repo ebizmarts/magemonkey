f0c6b6c0e998816f926e6d1575582dd054c4587d
Pablo Benitez <pablos@ebizmarts.com>
2012-02-06 12:57:19 -0200

    moving logic to Main Helper

--- app/code/community/Ebizmarts/MageMonkey/controllers/SignupController.php
+++ app/code/community/Ebizmarts/MageMonkey/controllers/SignupController.php
@@ -10,19 +10,6 @@ class Ebizmarts_MageMonkey_SignupController extends Mage_Core_Controller_Front_A
 	{
 		if($this->getRequest()->isPost()){
 
-			$guestEmail = $this->getRequest()->getPost('monkey_email');
-
-			//<state> param is an html serialized field containing the default form state
-			//before submission, we need to parse it as a request in order to save it to $odata and process it
-			parse_str($this->getRequest()->getPost('state'), $odata);
-
-			$curlists = (TRUE === array_key_exists('list', $odata)) ? $odata['list'] : array();
-			$lists    = $this->getRequest()->getPost('list', array());
-
-			$api       = Mage::getSingleton('monkey/api');
-			$customer  = Mage::helper('customer')->getCustomer();
-			$email     =  $guestEmail ? $guestEmail : $customer->getEmail();
-
 			$loggedIn = Mage::helper('customer')->isLoggedIn();
 			if(!$loggedIn && !Zend_Validate::is($email, 'EmailAddress')){
 				Mage::getSingleton('core/session')
@@ -31,53 +18,7 @@ class Ebizmarts_MageMonkey_SignupController extends Mage_Core_Controller_Front_A
 				return;
 			}
 
-
-
-			if( !empty($curlists) ){
-
-				//Handle Unsubscribe and groups update actions
-				foreach($curlists as $listId => $list){
-
-					if(FALSE === array_key_exists($listId, $lists)){
-
-						//Unsubscribe Email
-						$api->listUnsubscribe($listId, $email);
-
-					}else{
-
-						$groupings = $lists[$listId];
-						unset($groupings['subscribed']);
-						$customer->setMcListId($listId);
-						$customer->setListGroups($groupings);
-						$mergeVars = Mage::helper('monkey')->getMergeVars($customer);
-
-						//Handle groups update
-						$api->listUpdateMember($listId, $email, $mergeVars);
-
-					}
-
-				}
-
-			}
-
-			//Subscribe to new lists
-			$subscribe = array_diff_key($lists, $curlists);
-			if( !empty($subscribe) ){
-
-				foreach($subscribe as $listId => $slist){
-
-					$groupings = $lists[$listId];
-					unset($groupings['subscribed']);
-					$customer->setListGroups($groupings);
-					$customer->setMcListId($listId);
-					$mergeVars = Mage::helper('monkey')->getMergeVars($customer);
-
-					$api->listSubscribe($listId, $email, $mergeVars, 'html', ($loggedIn ? false : true));
-
-				}
-
-			}
-
+			Mage::helper('monkey')->handlePost($this->getRequest(), $this->getRequest()->getPost('monkey_email'));
 		}
 
 		$this->_redirect($this->_getRedirectPath());
