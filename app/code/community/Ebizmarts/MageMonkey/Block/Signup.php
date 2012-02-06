350519cf2439e39ec5b025ca43080ed737f7de87
Pablo Benitez <pablos@ebizmarts.com>
2012-02-06 10:18:32 -0200

    adding new files

---
+++ app/code/community/Ebizmarts/MageMonkey/Block/Signup.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Signup standalone block to be used on different places
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_MageMonkey
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ */
+class Ebizmarts_MageMonkey_Block_Signup extends Ebizmarts_MageMonkey_Block_Lists
+{
+
+}
