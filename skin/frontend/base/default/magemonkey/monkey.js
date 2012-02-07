ae420080f0dc11238a5cbcb4c0669a06e1d34e04
Pablo Benitez <pablos@ebizmarts.com>
2012-02-07 16:24:45 -0200

    in Dashboard, change "edit" link for "Newsletters"

--- skin/frontend/base/default/magemonkey/monkey.js
+++ skin/frontend/base/default/magemonkey/monkey.js
@@ -7,10 +7,16 @@ removeManageNewsletter = function(){
 
 document.observe("dom:loaded", function() {
 
-	var monkeyEnabled = $$('div.block-content ul li a[href*="monkey/customer_account/index"]').length;
+	var monkeyEnabled = $$('div.block-content ul li a[href*="monkey/customer_account/index"]');
 
-	if(monkeyEnabled){
+	if(monkeyEnabled.length){
 		removeManageNewsletter();
+
+		//If in Dashboard, change "edit" link for "Newsletters"
+		var editLink = $$('div.my-account a[href*="newsletter/manage"]');
+		if(editLink.length){
+			editLink.first().writeAttribute('href', monkeyEnabled.first().readAttribute('href'));
+		}
 	}
 
 });
