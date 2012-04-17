a8da99af3b729f58eafd117936196b845b95f908
Pablo Benitez <pablos@ebizmarts.com>
2012-04-17 10:49:27 -0300

    Export grid store column

--- app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Bulksync/QueueExport/Grid.php
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/Bulksync/QueueExport/Grid.php
@@ -86,14 +86,18 @@ class Ebizmarts_MageMonkey_Block_Adminhtml_Bulksync_QueueExport_Grid extends Mag
             'type'  => 'datetime'
         ));
 
+        $views = Mage::getModel('core/store')->getCollection()->toOptionHash();
+        array_unshift($views, Mage::helper('monkey')->__('All store views') );
+
         $this->addColumn('store_id',
             array(
-                'header'=> Mage::helper('monkey')->__('Websites'),
+                'header'=> Mage::helper('monkey')->__('Store'),
                 'width' => '100px',
                 'sortable'  => false,
+                'filter'  => false,
                 'index'     => 'store_id',
                 'type'      => 'options',
-                'options'   => Mage::getModel('core/website')->getCollection()->toOptionHash(),
+                'options'   => $views,
         ));
 
         $this->addColumn('action',
