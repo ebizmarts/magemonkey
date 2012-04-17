f036fdd2208a76aa765bb1b6339786376bce6357
Pablo Benitez <pablos@ebizmarts.com>
2012-04-17 10:50:36 -0300

    add store_id only to export table

--- app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-1.0.10-1.0.11.php
+++ app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-1.0.10-1.0.11.php
@@ -3,8 +3,5 @@
 $installer = $this;
 
 $installer->getConnection()->addColumn(
-    $installer->getTable('magemonkey_bulksync_import'), 'store_id', 'SMALLINT(5) unsigned NULL'
-);
-$installer->getConnection()->addColumn(
     $installer->getTable('magemonkey_bulksync_export'), 'store_id', 'SMALLINT(5) unsigned NULL'
 );
