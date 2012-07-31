a311625cc172a73e79f11bb36c5bd7031061b435
Luis Davyt <luisd@ebizmarts.com>
2012-07-31 15:40:04 -0300

    fix update customer function

--- app/code/community/Ebizmarts/MageMonkey/Model/Observer.php
+++ app/code/community/Ebizmarts/MageMonkey/Model/Observer.php
@@ -351,8 +351,9 @@ class Ebizmarts_MageMonkey_Model_Observer
 				$api->listUpdateMember($listId, $oldEmail, $mergeVars);
 			}
 		}
+		$request = Mage::app()->getRequest();
 		//Unsubscribe when update customer from admin
-		if (!isset($post['subscription']) && Mage::getSingleton('admin/session')->isLoggedIn()) {
+		if (!isset($post['subscription']) && && $request->getActionName() == 'save' && $request->getControllerName() == 'customer' && $request->getModuleName() == (string)Mage::getConfig()->getNode('admin/routers/adminhtml/args/frontName')) {
                  $subscriber = Mage::getModel('newsletter/subscriber')
                                ->loadByEmail($customer->getEmail());
                  $subscriber->setImportMode(TRUE)->unsubscribe();
