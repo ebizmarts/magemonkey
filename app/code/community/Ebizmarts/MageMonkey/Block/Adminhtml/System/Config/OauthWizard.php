3f15980fc57b69c315550ca8d82cd27b655ae06a
Pablo Benitez <pablo@ebizmarts.com>
2012-09-10 10:37:43 -0300

    changing code comments

--- app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/System/Config/OauthWizard.php
+++ app/code/community/Ebizmarts/MageMonkey/Block/Adminhtml/System/Config/OauthWizard.php
@@ -1,31 +1,11 @@
 <?php
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Paypal
- * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 
 /**
- * Custom renderer for PayPal API credentials wizard popup
+ * Custom renderer for Oauth2 authorization wizard popup
+ * 
+ * @category   Ebizmarts
+ * @package    Ebizmarts_MageMonkey
+ * @author     Ebizmarts Team <info@ebizmarts.com>
  */
 class Ebizmarts_MageMonkey_Block_Adminhtml_System_Config_OauthWizard extends Mage_Adminhtml_Block_System_Config_Form_Field {
 
