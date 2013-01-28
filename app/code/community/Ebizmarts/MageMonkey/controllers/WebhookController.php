170e3d3b523f34193bc91088580ab53179fe7b26
Luis Davyt <luisd@ebizmarts.com>
2013-01-28 17:09:39 -0200

    remove validation of wkey, fail in case of multiple websites

--- app/code/community/Ebizmarts/MageMonkey/controllers/WebhookController.php
+++ app/code/community/Ebizmarts/MageMonkey/controllers/WebhookController.php
@@ -35,7 +35,7 @@ class Ebizmarts_MageMonkey_WebhookController extends Mage_Core_Controller_Front_
 		$myKey = Mage::helper('monkey')->getWebhooksKey(null, $data['list_id']);
 
 		//Validate "wkey" GET parameter
-		if (($requestKey == $myKey) && ($this->getRequest()->getPost('type'))) {
+		if ($this->getRequest()->getPost('type')) {
 			Mage::getModel('monkey/monkey')->processWebhookData($this->getRequest()->getPost());
 		} else {
 		    Mage::helper('monkey')->log($this->__('WebHook Key invalid! Key Request: %s. My Key: %s', $requestKey, $myKey));
