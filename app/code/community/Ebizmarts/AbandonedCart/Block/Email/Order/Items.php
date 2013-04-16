defe7b2e15d1d714d80c0d56d12c2665446d3f2e
Luis Davyt <luisd@ebizmarts.com>
2013-04-16 17:01:25 -0300

    fix email template for abandoned cart adding tax and skus

--- app/code/community/Ebizmarts/AbandonedCart/Block/Email/Order/Items.php
+++ app/code/community/Ebizmarts/AbandonedCart/Block/Email/Order/Items.php
@@ -6,4 +6,17 @@ class Ebizmarts_AbandonedCart_Block_Email_Order_Items extends Mage_Sales_Block_I
     {
         $this->setTemplate('ebizmarts_abandonedcart/email_order_items.phtml');
     }
+
+    public function getTax($_item)
+    {
+		if (Mage::helper('tax')->displayCartPriceInclTax()){
+			$subtotal = Mage::helper('tax')->__('Incl. Tax') . ' : ' .Mage::helper('checkout')->formatPrice($_item['row_total_incl_tax']);
+		} elseif(Mage::helper('tax')->displayCartBothPrices()) {
+			$subtotal = Mage::helper('tax')->__('Excl. Tax') . ' : ' . Mage::helper('checkout')->formatPrice($_item['row_total']) . '<br>'. Mage::helper('tax')->__('Incl. Tax') . ' : ' . Mage::helper('checkout')->formatPrice($_item['row_total_incl_tax']);
+		} else {
+			$subtotal = Mage::helper('tax')->__('Excl. Tax') . ' : ' . Mage::helper('checkout')->formatPrice($_item['row_total']);
+		}
+		return $subtotal;
+    }
+
 }
