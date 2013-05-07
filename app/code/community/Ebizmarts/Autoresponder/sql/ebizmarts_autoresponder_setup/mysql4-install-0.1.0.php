3c8e37f91a866a895b36e782a124657322c1b43e
Gonzalo Dominguez <gonzalo@ebizmarts.com>
2013-05-07 14:57:52 -0300

    Autoresponder

---
+++ app/code/community/Ebizmarts/Autoresponder/sql/ebizmarts_autoresponder_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Author : Ebizmarts <info@ebizmarts.com>
+ * Date   : 4/28/13
+ * Time   : 11:20 AM
+ * File   : ${FILE_NAME}
+ * Module : ${PROJECT_NAME}
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
