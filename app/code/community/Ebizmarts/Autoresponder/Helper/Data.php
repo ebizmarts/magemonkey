6ce31c5e61a31518739a29eb0098e3b00df35119
gonzalo <gonzalo@ebizmarts.com>
2013-06-27 15:32:23 -0300

    customer myaccount

--- app/code/community/Ebizmarts/Autoresponder/Helper/Data.php
+++ app/code/community/Ebizmarts/Autoresponder/Helper/Data.php
@@ -16,17 +16,19 @@ class Ebizmarts_Autoresponder_Helper_Data extends Mage_Core_Helper_Abstract {
         $lists['abandonedcart'] = array('listname'=>'Abandoned Carts List');
         foreach ($lists as $key =>$data) {
             if(isset($data['listname'])) {
-                $types[$key]['listname'] = (string)$data['listname'];
-                $collection = Mage::getModel('ebizmarts_autoresponder/unsubscribe')->getCollection();
-                $email = $this->_getEmail();
-                $collection->addFieldToFilter('main_table.email',array('eq'=>$email))
-                    ->addFieldToFilter('main_table.list',array('eq'=>$key))
-                    ->addFieldToFilter('main_table.store_id',array('eq'=>$storeId));
-                if($collection->getSize() > 0) {
-                    $types[$key]['checked'] = "";
-                }
-                else {
-                    $types[$key]['checked'] = "checked";
+                if(Mage::getStoreConfig("ebizmarts_autoresponder/$key/active",$storeId)||($key=='abandonedcart'&&Mage::getStoreConfig("ebizmarts_abandonedcart/general/active",$storeId))) {
+                    $types[$key]['listname'] = (string)$data['listname'];
+                    $collection = Mage::getModel('ebizmarts_autoresponder/unsubscribe')->getCollection();
+                    $email = $this->_getEmail();
+                    $collection->addFieldToFilter('main_table.email',array('eq'=>$email))
+                        ->addFieldToFilter('main_table.list',array('eq'=>$key))
+                        ->addFieldToFilter('main_table.store_id',array('eq'=>$storeId));
+                    if($collection->getSize() > 0) {
+                        $types[$key]['checked'] = "";
+                    }
+                    else {
+                        $types[$key]['checked'] = "checked";
+                    }
                 }
             }
         }
