fb4f019d69d1d257e12e37ab477c2cb3dfc2f399
gonzalo <gonzalo@ebizmarts.com>
2013-06-28 14:43:27 -0300

    check if Mage_Review output is activated

--- app/code/community/Ebizmarts/Autoresponder/Block/Adminhtml/System/Config/Review/Fieldset/Hint.php
+++ app/code/community/Ebizmarts/Autoresponder/Block/Adminhtml/System/Config/Review/Fieldset/Hint.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Author : Ebizmarts <info@ebizmarts.com>
- * Date   : 6/25/13
- * Time   : 2:15 PM
+ * Date   : 6/28/13
+ * Time   : 2:35 PM
  * File   : Hint.php
  * Module : Ebizmarts_Magemonkey
  */
-class Ebizmarts_Autoresponder_Block_Adminhtml_System_Config_Birthday_Fieldset_Hint
+class Ebizmarts_Autoresponder_Block_Adminhtml_System_Config_Review_Fieldset_Hint
     extends Mage_Adminhtml_Block_Abstract
     implements Varien_Data_Form_Element_Renderer_Interface
 {
-    protected $_template = 'ebizmarts/autoresponder/system/config/birthday/fieldset/hint.phtml';
+    protected $_template = 'ebizmarts/autoresponder/system/config/review/fieldset/hint.phtml';
 
     /**
      * @param Varien_Data_Form_Element_Abstract $element
