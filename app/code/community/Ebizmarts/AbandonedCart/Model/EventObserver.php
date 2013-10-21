72685286c832934228ebc4f9db8ea30f664dfb85
gonzalo <gonzalo@ebizmarts.com>
2013-10-21 14:26:54 -0200

    fix for wrong default store

--- app/code/community/Ebizmarts/AbandonedCart/Model/EventObserver.php
+++ app/code/community/Ebizmarts/AbandonedCart/Model/EventObserver.php
@@ -10,10 +10,20 @@ class Ebizmarts_AbandonedCart_Model_EventObserver
 {
     public function saveConfig(Varien_Event_Observer $o)
     {
-        $store  = is_null($o->getEvent()->getStore()) ? 'default': $o->getEvent()->getStore();
+        if(Mage::app()->getRequest()->getParam('store')) {
+            $scope = 'store';
+        }
+        elseif(Mage::app()->getRequest()->getParam('website')) {
+            $scope = 'website';
+        }
+        else {
+            $scope = 'default';
+        }
+
+        $store  = is_null($o->getEvent()->getStore()) ? Mage::app()->getDefaultStoreView()->getCode(): $o->getEvent()->getStore();
         if(!Mage::helper('mandrill')->useTransactionalService()) {
             $config =  new Mage_Core_Model_Config();
-            $config->saveConfig(Ebizmarts_AbandonedCart_Model_Config::ACTIVE,false,"default",$store);
+            $config->saveConfig(Ebizmarts_AbandonedCart_Model_Config::ACTIVE,false,$scope,$store);
             Mage::getConfig()->cleanCache();
         }
 
