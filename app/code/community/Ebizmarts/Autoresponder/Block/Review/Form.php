0eacbcdb4dba2a021d03c7fdb4ea8ee1d51ed625
gonzalo <gonzalo@ebizmarts.com>
2013-10-25 15:16:11 -0200

    fix for reviews

---
+++ app/code/community/Ebizmarts/Autoresponder/Block/Review/Form.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author : Ebizmarts <info@ebizmarts.com>
+ * Date   : 10/24/13
+ * Time   : 7:34 PM
+ * File   : Form.php
+ * Module : Ebizmarts_Magemonkey
+ */
+class Ebizmarts_Autoresponder_Block_Review_Form extends Mage_Review_Block_Form
+{
+    public function getAction()
+    {
+        Mage::log(__METHOD__);
+        $productId = Mage::app()->getRequest()->getParam('id', false);
+        $token = Mage::app()->getRequest()->getParam('token', false);
+        return Mage::getUrl('review/product/post', array('id' => $productId, 'token' => $token));
+    }
+
+}
