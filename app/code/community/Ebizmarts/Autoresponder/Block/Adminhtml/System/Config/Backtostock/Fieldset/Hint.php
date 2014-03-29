dba00878174e7b978a964aae5f15b6ad44781d14
Martin Hansen <martin.hansen@gmail.com>
2014-03-29 11:18:07 -0300

    Added hint for Back to Stock configuration

---
+++ app/code/community/Ebizmarts/Autoresponder/Block/Adminhtml/System/Config/Backtostock/Fieldset/Hint.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_Autoresponder
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ */
+
+class Ebizmarts_Autoresponder_Block_Adminhtml_System_Config_Backtostock_Fieldset_Hint
+    extends Mage_Adminhtml_Block_Abstract
+    implements Varien_Data_Form_Element_Renderer_Interface
+{
+    protected $_template = 'ebizmarts/autoresponder/system/config/backtostock/fieldset/hint.phtml';
+
+    /**
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        return $this->toHtml();
+    }
+
+
+}
