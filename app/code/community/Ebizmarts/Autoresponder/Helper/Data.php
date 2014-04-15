2bc59275f8e21c958df5a755ca12a0bf77cb7c6e
Martin Hansen <martin.hansen@gmail.com>
2014-04-15 13:05:50 -0300

    Added log facility for Ebizmarts_Autoresponder

--- app/code/community/Ebizmarts/Autoresponder/Helper/Data.php
+++ app/code/community/Ebizmarts/Autoresponder/Helper/Data.php
@@ -1,12 +1,28 @@
 <?php
+
 /**
- * Author : Ebizmarts <info@ebizmarts.com>
- * Date   : 4/28/13
- * Time   : 11:20 AM
- * File   : Data.php
- * Module : Ebizmarts_Magemonkey
- */ 
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_Autoresponder
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ */
+
 class Ebizmarts_Autoresponder_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    /**
+     * Logging facility
+     *
+     * @param mixed $data Message to save to file
+     * @param string $filename log filename, default is <Ebizmarts_Autoresponder.log>
+     * @return Mage_Core_Model_Log_Adapter
+     */
+    public function log($data, $filename = 'Ebizmarts_Autoresponder.log')
+    {
+        if($this->config('enable_log') != 0) {
+            return Mage::getModel('core/log_adapter', $filename)->log($data);
+        }
+    }
 
     public function getLists()
     {
@@ -37,5 +53,13 @@ class Ebizmarts_Autoresponder_Helper_Data extends Mage_Core_Helper_Abstract {
     protected function _getEmail()
     {
         return Mage::helper('customer')->getCustomer()->getEmail();
+    }
+
+    public function isBacktoStockEnabledForGuest()
+    {
+        $storeId = Mage::app()->getStore()->getStoreId();
+        $allowGuests = Mage::getStoreConfig("ebizmarts_autoresponder/backtostock/allow_guests", $storeId);
+
+        return $allowGuests;
     }
 }
