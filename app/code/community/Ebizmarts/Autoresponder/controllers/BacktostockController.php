c7b543ce1644aeb58a707f21b0c7132fe5a28362
Martin Hansen <martin.hansen@gmail.com>
2014-05-04 17:09:02 -0300

    refactor controller logic

--- app/code/community/Ebizmarts/Autoresponder/controllers/BacktostockController.php
+++ app/code/community/Ebizmarts/Autoresponder/controllers/BacktostockController.php
@@ -20,19 +20,67 @@ class Ebizmarts_Autoresponder_BacktostockController extends Mage_Core_Controller
 
             $email = $params['subscriber_email'];
             $productId = $params['product_id'];
+            $alertId = false;
 
-            $backtostock = Mage::getModel('ebizmarts_autoresponder/backtostock');
-            $backtostock
-                ->setProductId($productId)
+            $stockAlertCollection = Mage::getModel('ebizmarts_autoresponder/backtostockalert')->getCollection();
+            $stockAlertCollection
+                ->addFieldToFilter('main_table.product_id', array('eq'=>$productId))
+                ->addFieldToFilter('main_table.is_active', array('eq'=> 1))
+            ;
+
+            // Check if we already have this Product ID in alert table, otherwise insert a new one
+            if($stockAlertCollection->getSize() == 0) {
+                $stockAlert = Mage::getModel('ebizmarts_autoresponder/backtostockalert');
+                $stockAlert
+                    ->setProductId($productId)
+                    ->setIsActive(1)
+                ;
+
+                $stockAlert->save();
+
+                $alertId = $stockAlert->getAlertId();
+
+                Mage::helper('ebizmarts_autoresponder')->log('New Stock Alert for product #'. $productId . ' was saved.');
+
+            } else {
+                // Retrieve existing Stock Alert ID
+                $alertId = $stockAlertCollection->getFirstItem()->getAlertId();
+            }
+
+            // Create new notification for this subscriber
+            $backStock = Mage::getModel('ebizmarts_autoresponder/backtostock');
+            $backStock
+                ->setAlertId($alertId)
                 ->setEmail($email)
             ;
-            $backtostock->save();
+            $backStock->save();
+
+
+            //@TODO add configuration option to ask if Admin wants to redirect to Product's View
+            // Redirects to Product's View
+            /*
+            if($productId) {
+                $currentProduct = Mage::getModel('catalog/product')->load($productId);
+
+                if($currentProduct && $currentProduct->getProductUrl()) {
+                    $redirect = $currentProduct->getProductUrl();
+                }
+
+            }
+            */
 
             Mage::getSingleton('core/session')
                 ->addSuccess($this->__('You have been subscribed successfully!'));
+
+        } else {
+            // Something went wrong and some of the needed params didn't arrived
+            Mage::helper('ebizmarts_autoresponder')->log('ERROR - Cannot extract email or ProductID from params. Ensure that Back to stock form is well rendered and has this two fields "subscriber_email", "product_id".');
+
+            Mage::getSingleton('core/session')
+                ->addError($this->__('We cannot subscribe you at this moment. Please, check again later.'));
         }
 
-        // Decide where the User will be redirected
+        // Redirect User
         $this->_redirectUrl($redirect);
     }
 
