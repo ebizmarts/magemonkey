2ff027c6b17910282ce08b11e83cb3a98ce4dc92
Gonzalo <gonzalo@ebizmarts.com>
2014-08-14 23:23:57 -0300

    Mandrill 2.0

---
+++ lib/Mandrill/Mandrill/Internal.php
@@ -0,0 +1,10 @@
+<?php
+
+class Mandrill_Internal {
+    public function __construct(Mandrill_Mandrill $master) {
+        $this->master = $master;
+    }
+
+}
+
+
---
+++ lib/Mandrill/Message.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Author : Ebizmarts <info@ebizmarts.com>
+ * Date   : 8/14/14
+ * Time   : 6:48 PM
+ * File   : Message.php
+ * Module : Ebizmarts_Mandrill
+ */
+class Mandrill_Message extends Mandrill_Mandrill
+{
+    protected $_attachments = array();
+    protected $_bcc = array();
+    public function createAttachment($body,
+                                     $mimeType    = Zend_Mime::TYPE_OCTETSTREAM,
+                                     $disposition = Zend_Mime::DISPOSITION_ATTACHMENT,
+                                     $encoding    = Zend_Mime::ENCODING_BASE64,
+                                     $filename    = null)
+    {
+        $att = array('type' => $mimeType,'name' => $filename,'content'=> base64_encode($body));
+        array_push($this->_attachments,$att);
+    }
+    public function log($m)
+    {
+        $storeId = Mage::app()->getStore()->getId();
+        if(Mage::getStoreConfig(Ebizmarts_Mandrill_Model_System_Config::ENABLE_LOG,$storeId))
+        {
+            Mage::log($m,Zend_Log::INFO,'Mandrill.log');
+        }
+    }
+    public function getAttachments()
+    {
+        return $this->_attachments;
+    }
+    public function addBcc($bcc)
+    {
+        $storeId = Mage::app()->getStore()->getId();
+        if(is_array($bcc))
+        {
+            foreach($bcc as $email)
+            {
+                array_push($this->_bcc,$email);
+            }
+        }
+        else
+        {
+            array_push($this->_bcc,$bcc);
+        }
+    }
+    public function getBcc()
+    {
+        return $this->_bcc;
+    }
+}
