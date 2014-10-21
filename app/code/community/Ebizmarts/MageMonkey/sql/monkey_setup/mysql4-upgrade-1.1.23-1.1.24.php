ada7232e13a218c9b133fbc243665c38009af52c
santisp <santiago@ebizmarts.com>
2014-10-21 11:46:40 -0200

    Changed Mandrill version to 2.0.1

--- app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-1.1.23-1.1.24.php
+++ app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-1.1.23-1.1.24.php
@@ -5,11 +5,8 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-
     ALTER TABLE magemonkey_async_subscribers CHANGE proccessed processed INT;
-
     ALTER TABLE magemonkey_async_orders CHANGE proccessed processed INT;
-
 ");
 
 $installer->endSetup();
