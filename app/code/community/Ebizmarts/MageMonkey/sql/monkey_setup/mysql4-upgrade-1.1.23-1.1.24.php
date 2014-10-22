450cdb1059e7a07be8f99b5faab52d4fc482264d
santisp <santiago@ebizmarts.com>
2014-10-22 13:31:16 -0200

    compatibility with table prefix

--- app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-1.1.23-1.1.24.php
+++ app/code/community/Ebizmarts/MageMonkey/sql/monkey_setup/mysql4-upgrade-1.1.23-1.1.24.php
@@ -5,8 +5,8 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-    ALTER TABLE {$this->getTable('magemonkey_async_subscribers')} CHANGE proccessed processed INT;
-    ALTER TABLE {$this->getTable('magemonkey_async_orders')} CHANGE proccessed processed INT;
+    ALTER TABLE `{$this->getTable('magemonkey_async_subscribers')}` CHANGE `proccessed` `processed` INT;
+    ALTER TABLE `{$this->getTable('magemonkey_async_orders')}` CHANGE `proccessed` `processed` INT;
 ");
 
 $installer->endSetup();
