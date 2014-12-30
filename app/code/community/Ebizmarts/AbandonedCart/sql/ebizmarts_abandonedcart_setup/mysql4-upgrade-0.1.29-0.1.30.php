9d4503807ab4d1d4b4db9373ff9c2e66e9ab8068
santisp <santiago@ebizmarts.com>
2014-12-30 14:30:13 -0200

    Creating popup insist feature.

--- app/code/community/Ebizmarts/AbandonedCart/sql/ebizmarts_abandonedcart_setup/mysql4-upgrade-0.1.29-0.1.30.php
+++ app/code/community/Ebizmarts/AbandonedCart/sql/ebizmarts_abandonedcart_setup/mysql4-upgrade-0.1.29-0.1.30.php
@@ -13,13 +13,11 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-
 	CREATE TABLE IF NOT EXISTS `{$this->getTable('ebizmarts_abandonedcart_popup')}` (
-	  `id` int(10) unsigned NOT NULL auto_increment,
-	  `customer_email` varchar(255) DEFAULT NULL,
-	  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-	  `counter`  int(10),
-	  PRIMARY KEY  (`id`)
+	  `id` INT(10) unsigned NOT NULL auto_increment,
+	  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	  `counter` INT(10),
+	  PRIMARY KEY (`id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ");
