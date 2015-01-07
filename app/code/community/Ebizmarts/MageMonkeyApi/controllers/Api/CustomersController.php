b7dac19a678d2d0cdb8e2253e2aa9274c5b54572
Pablo S. Benitez <pablos.benitez@gmail.com>
2015-01-06 22:34:40 -0200

    customers

---
+++ app/code/community/Ebizmarts/MageMonkeyApi/controllers/Api/CustomersController.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'Ebizmarts/MageMonkeyApi/controllers/ApiController.php';
+
+class Ebizmarts_MageMonkeyApi_Api_CustomersController extends Ebizmarts_MageMonkeyApi_ApiController {
+
+	/**
+	 * Return last 5 orders.
+	 */
+	public function indexAction() {
+
+		$id = $this->getRequest()->getParam('id');
+
+		if($id) {
+			$customer = Mage::getModel('customer/customer')->load((int)$id);
+
+			if(!$customer->getId()) {
+				$this->_setClientError(400, 4006);
+	        	return;
+			}
+			else {
+				$this->_setSuccess(200, $this->_customerData($customer));
+	        	return;
+			}
+
+		}
+		else {
+
+			$customerCollection = Mage::getResourceModel('customer/customer_collection')
+            ->addNameToSelect()
+			->setOrder('created_at', 'DESC')
+			->load();
+
+			//echo (string)$customerCollection->getSelect();
+
+			$ret = array();
+
+			foreach($customerCollection as $order) {
+				$ret []= $this->_customerData($order);
+			}
+
+			$this->_setSuccess(200, $ret);
+	        return;
+
+    	}
+	}
+
+	protected function _customerData(Mage_Customer_Model_Customer $customer) {
+        $result = array();
+        $result['customer_id']              = (int) $customer->getId();
+        $result['firstname']                = $customer->getFirstname();
+        $result['lastname']                 = $customer->getLastname();
+        $result['email']                    = $customer->getEmail();
+        $result['website_id']               = (int) $customer->getWebsiteId();
+        $result['group_id']                 = (int) $customer->getGroupId();
+
+		return $result;
+	}
+
+}
