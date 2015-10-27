7ed4e068c463c9ceffd69982c62c166fac43d9ed
santisp <santiago@ebizmarts.com>
2015-10-27 14:56:37 -0300

    Added upgrade to allow custom blocks after new Magento patch. Changed the modules to be loaded after Magento's core.

---
+++ app/code/community/Ebizmarts/AbandonedCart/sql/ebizmarts_abandonedcart_setup/mysql4-upgrade-1.1.35-1.1.36.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *
+ * @category   Ebizmarts
+ * @package    Ebizmarts_Autoresponder
+ * @author     Ebizmarts Team <info@ebizmarts.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ */
+
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->getConnection()->insertMultiple(
+    $installer->getTable('admin/permission_block'),
+    array(
+        array('block_name' => 'ebizmarts_autoresponder/email_backtostock_item', 'is_allowed' => 1)
+    )
+);
+
+$installer->endSetup();
