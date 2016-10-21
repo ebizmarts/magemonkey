350e99342d8f9ac2e870ccf0238b46e370ddc15d
Santiago <santiago@ebizmarts.com>
2016-10-21 11:46:18 -0300

    Added support for grouped product with image on the simple products in stead of the parent.

--- app/code/community/Ebizmarts/AbandonedCart/Block/Email/Order/Items.php
+++ app/code/community/Ebizmarts/AbandonedCart/Block/Email/Order/Items.php
@@ -31,7 +31,7 @@ class Ebizmarts_AbandonedCart_Block_Email_Order_Items extends Mage_Sales_Block_I
         $product = Mage::getModel('catalog/product')
             ->load($_item->getProductId());
         $imageUrl = $product->getThumbnailUrl();
-        if ($product->getImage() == "no_selection" && $product->getTypeId() == "configurable") {
+        if ($product->getImage() == "no_selection" && $product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE) {
             $conf = Mage::getModel('catalog/product_type_configurable')->setProduct($product);
             $simpleCollection = $conf->getUsedProductCollection()->addAttributeToSelect('*')->addFilterByRequiredOptions();
             foreach ($simpleCollection as $simpleProduct) {
@@ -39,8 +39,14 @@ class Ebizmarts_AbandonedCart_Block_Email_Order_Items extends Mage_Sales_Block_I
                     $imageUrl = $simpleProduct->getThumbnailUrl();
                 }
             }
+        } elseif ($product->getImage() == "no_selection" && $product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_GROUPED) {
+            $associatedProducts = $product->getTypeInstance(true)->getAssociatedProducts($product);
+            foreach ($associatedProducts as $simpleProduct) {
+                if ($simpleProduct->getImage() != "no_selection") {
+                    $imageUrl = $simpleProduct->getThumbnailUrl();
+                }
+            }
         }
         return $imageUrl;
     }
-
 }
