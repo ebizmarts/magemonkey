1a7fcbfac62124e9fb3c7f27adf2e4add84729c2
Santiago <santiago@ebizmarts.com>
2016-12-05 12:05:20 -0300

    Fix for website_id filter on coupons. closes #158

--- app/code/community/Ebizmarts/AbandonedCart/Model/Cron.php
+++ app/code/community/Ebizmarts/AbandonedCart/Model/Cron.php
@@ -403,7 +403,7 @@ class Ebizmarts_AbandonedCart_Model_Cron
         $websiteid = Mage::getModel('core/store')->load($store)->getWebsiteId();
         $collection = Mage::getModel('salesrule/rule')->getCollection()
             ->addFieldToFilter('name', array('like' => 'Abandoned coupon ' . $email))
-            ->addFieldToFilter('website_id', array('eq' => $websiteid));
+            ->addFieldToFilter('website_ids', array('eq' => $websiteid));
         if (!count($collection)) {
 
             $fromDate = date("Y-m-d");
